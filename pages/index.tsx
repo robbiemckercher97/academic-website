7518273b97b7e4f7ad25fd338eb239eaf12c4ff0
robbiemckercher97 <robbiemckercher@gmail.com>
2026-09-10 19:58:58 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -89,7 +89,7 @@ const Home: NextPage = () => {
           <div className={styles.heroSection}>
             <div className={styles.photoContainer}>
               <img
-                src="/me.jpg"
+                src="/me.jpeg"
                 alt="Robert McKercher"
                 className={styles.photo}
               />
