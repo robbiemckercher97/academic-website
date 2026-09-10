3f9baffba38d88094ff633b480e74a3ba281954f
robbiemckercher97 <robbiemckercher@gmail.com>
2026-09-10 19:44:23 -0400

    Update image source in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -89,7 +89,7 @@ const Home: NextPage = () => {
           <div className={styles.heroSection}>
             <div className={styles.photoContainer}>
               <img
-                src="https://via.placeholder.com/180"
+                src="/me.jpg"
                 alt="Robert McKercher"
                 className={styles.photo}
               />
