0dd83b44a1fa3459dae695d44e1dd9e39c21e4fa
robbiemckercher97 <robbiemckercher@gmail.com>
2026-09-10 21:50:41 -0400

    Update CV page to use McKercher_CV_2016.pdf

--- pages/cv.tsx
+++ pages/cv.tsx
@@ -41,7 +41,7 @@ const CV: NextPage = () => {
         <div className={styles.section}>
           <div className={styles.sectionTitle}>Curriculum Vitae</div>
           <div className={styles.cvSection}>
-            <a href="/cv.pdf" className={styles.cvButton} download>
+            <a href="/McKercher_CV_2016.pdf" className={styles.cvButton} download>
               Download CV (PDF)
             </a>
           </div>
@@ -49,7 +49,7 @@ const CV: NextPage = () => {
           {/* Embedded PDF viewer */}
           <div className={styles.pdfContainer}>
             <iframe
-              src="/cv.pdf#toolbar=1"
+              src="/McKercher_CV_2016.pdf#toolbar=1"
               className={styles.pdfViewer}
               title="CV - Robert McKercher"
             />
