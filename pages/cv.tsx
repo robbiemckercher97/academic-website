f16d12e9e2d406e5ad5c387f5280cfe8af92a623
robbiemckercher97 <robbiemckercher@gmail.com>
2026-09-16 03:02:12 -0400

    Update cv.tsx

--- pages/cv.tsx
+++ pages/cv.tsx
@@ -131,9 +131,9 @@ const CVPage: NextPage = () => {
           </div>
           <ul className={styles.navLinks}>
             <li>
-              <a onClick={() => navigateTo('about')} style={{ cursor: 'pointer' }}>
-                About
-              </a>
+             <section id="about" style={{ scrollMarginTop: '150px' }}>
+                {/* Existing About content */}
+              </section>
             </li>
             <li>
               <a onClick={() => navigateTo('research')} style={{ cursor: 'pointer' }}>
